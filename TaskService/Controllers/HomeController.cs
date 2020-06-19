478777e64c203e5ab262101b87bf3ebbbfb1d8b2
Dinesh Kumar <dotnext24@gmail.com>
2020-06-19 14:59:23 +0530

    Add project files.

---
+++ TaskService/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskService.Controllers
+{
+
+   
+
+    public class HomeController : Controller
+    {
+
+       
+        public ActionResult Index()
+        {
+            
+            ViewBag.Title = "Home Page";
+            var ss = User.Identity.Name;
+
+            return View();
+        }
+    }
+}
